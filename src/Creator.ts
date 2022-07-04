8b27975fb96b4ca4bc4c1cc4284b4d624f9241d7
Juan Aguado <jaguadoromero@gmail.com>
2022-07-04 14:25:26 +0200

    Add strict_types option to settings

--- src/Creator.ts
+++ src/Creator.ts
@@ -2,8 +2,6 @@ import * as vscode from 'vscode'
 import * as fs from 'fs'
 import NamespaceResolver from './NamespaceResolver'
 import path = require('path')
-// Import settings file from extension
-import settings  from './settings.json'
 export default class Creator {
     readonly msgFileExists = "File already exists!"
 
@@ -16,67 +14,67 @@ export default class Creator {
             })
 
             if (!askedFolder || !askedFolder[0]) {
-                return;
+                return
             }
 
-            folder = askedFolder[0];
+            folder = askedFolder[0]
         }
 
         let name = await vscode.window.showInputBox({
             title: "New PHP " + this.capitalize(type),
             placeHolder: "Name",
             prompt: "Name of " + type
-        });
+        })
 
         if (!name) {
-            return;
+            return
         }
 
         let namespaceResolver: NamespaceResolver = new NamespaceResolver()
         let namespace = await namespaceResolver.resolve(folder.fsPath)
 
         if (namespace === undefined) {
-            return;
+            return
         }
 
         let filename = name.endsWith('.php') ? name : name + '.php'
         
-        let space_index: number = filename.indexOf(' ');
+        let space_index: number = filename.indexOf(' ')
         if (space_index > 0) {
-            filename = filename.substring(0, space_index) + '.php';
+            filename = filename.substring(0, space_index) + '.php'
         }
         
         let fullFilename = folder.fsPath + path.sep + filename
 
-        this.writeFile(type, name, fullFilename, namespace);
+        this.writeFile(type, name, fullFilename, namespace)
     }
 
     private writeFile(type: string, name: string, filename: string, namespace: string): void {
         if (fs.existsSync(filename)) {
-            vscode.window.showErrorMessage(this.msgFileExists);
+            vscode.window.showErrorMessage(this.msgFileExists)
             return
         }
 
-        let content = "<?php\n";
+        let content = "<?php\n"
 
-        if(settings.strict_types) {
-            content += "declare(strict_types=1);\n";
+        if(vscode.workspace.getConfiguration("phpCreateClass").get("strict_types")) {
+            content += "declare(strict_types=1);\n"
         }
 
-        content += "\n";
-        content += "namespace " + namespace + ";\n";
-        content += "\n";
-        content += type + " " + name + "\n";
-        content += "{\n\n}\n";
+        content += "\n"
+        content += "namespace " + namespace + ";\n"
+        content += "\n"
+        content += type + " " + name + "\n"
+        content += "{\n\n}\n"
 
-        fs.writeFileSync(filename, content);
+        fs.writeFileSync(filename, content)
 
         vscode.workspace.openTextDocument(vscode.Uri.file(filename)).then(file => {
-            vscode.window.showTextDocument(file);
+            vscode.window.showTextDocument(file)
         })
     }
 
     private capitalize(text: string): string {
-        return text.charAt(0).toUpperCase() + text.slice(1);
+        return text.charAt(0).toUpperCase() + text.slice(1)
     }
 }
